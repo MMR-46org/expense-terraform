a0addcfa8d350deb611e9fbeb35070cb56aa8a9e
madhan-46 <madhanmohanreddyeeda@gmail.com>
2024-02-10 01:38:14 -0500

    creating vpc and subnets using terraform

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,3 +1,19 @@
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
+
+
+  tags =  {
+    name = "${var.env}-${var.project_name}-vpc"
+  }
+}
+
+resource "aws_subnet" "main" {
+  count      = length(var.subnets_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(var.subnets_cidr,count.index)
+  availability_zone = element(var.az, count.index)
+
+  tags = {
+    Name = "subnet-$(count.index)"
+  }
 }
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,1 +1,7 @@
 variable "vpc_cidr" {}
+variable "env" {}
+variable  "project_name" {}
+
+variable  "subnets_cidr" {}
+variable  "az" {}
+
