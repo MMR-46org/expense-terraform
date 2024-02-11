024f69ff6b2400d4eb95961ae3ade19310a0eed0
madhan-46 <madhanmohanreddyeeda@gmail.com>
2024-02-11 13:52:58 -0500

    creating public and private subnets for the internet gateway

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,7 +1,8 @@
 variable "vpc_cidr" {}
 variable "env" {}
 variable  "project_name" {}
-
+variable "public_subnets_cidr" {}
+variable "private_subnets_cidr" {}
 variable  "subnets_cidr" {}
 variable  "az" {}
 
