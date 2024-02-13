1ead96c8c36750603e3b96336b7048b4556435f8
madhan-46 <madhanmohanreddyeeda@gmail.com>
2024-02-13 15:13:31 -0500

    creating subnets for the app,web and db and also creating routing table fro all this.

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -95,8 +95,9 @@ resource "aws_route_table_association"  "public" {
 
 
 
-resource "aws_route_table" "private" {
-  count               = length(var.private_subnets_cidr)
+
+resource "aws_route_table" "web" {
+  count               = length(var.web_subnets_cidr)
   vpc_id = aws_vpc.main.id
 
   route {
@@ -111,27 +112,111 @@ resource "aws_route_table" "private" {
 
 
   tags = {
-    Name = "private-rt-${count.index+1}"
+    Name = "web-rt-${count.index+1}"
   }
 }
 
 
-resource "aws_route_table_association"  "private" {
-  count = length(var.private_subnets_cidr)
-  route_table_id = lookup(element(aws_route_table.private, count.index), "id", null)
-  subnet_id      = lookup(element(aws_subnet.private, count.index),  "id", null)
+resource "aws_route_table_association"  "web" {
+  count = length(var.web_subnets_cidr)
+  route_table_id = lookup(element(aws_route_table.web, count.index), "id", null)
+  subnet_id      = lookup(element(aws_subnet.web, count.index),  "id", null)
 
 }
 
 
-resource "aws_subnet" "private" {
-  count               = length(var.private_subnets_cidr)
+resource "aws_subnet" "web" {
+  count               = length(var.web_subnets_cidr)
   vpc_id              = aws_vpc.main.id
-  cidr_block          = element(var.private_subnets_cidr,count.index)
+  cidr_block          = element(var.web_subnets_cidr,count.index)
   availability_zone   = element(var.az, count.index)
 
   tags = {
-    Name = "private_subnet-${count.index+1}"
+    Name = "web_subnet-${count.index+1}"
+  }
+}
+
+
+
+resource "aws_route_table" "app" {
+  count               = length(var.app_subnets_cidr)
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = lookup(element(aws_nat_gateway.main, count.index), "id" , null)
+  }
+
+  route {
+    cidr_block = data.aws_vpc.default.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+  }
+
+
+  tags = {
+    Name = "app-rt-${count.index+1}"
+  }
+}
+
+
+resource "aws_route_table_association"  "app" {
+  count = length(var.app_subnets_cidr)
+  route_table_id = lookup(element(aws_route_table.app, count.index), "id", null)
+  subnet_id      = lookup(element(aws_subnet.app, count.index),  "id", null)
+
+}
+
+
+resource "aws_subnet" "app" {
+  count               = length(var.app_subnets_cidr)
+  vpc_id              = aws_vpc.main.id
+  cidr_block          = element(var.app_subnets_cidr,count.index)
+  availability_zone   = element(var.az, count.index)
+
+  tags = {
+    Name = "app_subnet-${count.index+1}"
+  }
+}
+
+
+
+resource "aws_route_table" "db" {
+  count               = length(var.db_subnets_cidr)
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = lookup(element(aws_nat_gateway.main, count.index), "id" , null)
+  }
+
+  route {
+    cidr_block = data.aws_vpc.default.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+  }
+
+
+  tags = {
+    Name = "db-rt-${count.index+1}"
+  }
+}
+
+
+resource "aws_route_table_association"  "db" {
+  count = length(var.db_subnets_cidr)
+  route_table_id = lookup(element(aws_route_table.db, count.index), "id", null)
+  subnet_id      = lookup(element(aws_subnet.db, count.index),  "id", null)
+
+}
+
+
+resource "aws_subnet" "db" {
+  count               = length(var.db_subnets_cidr)
+  vpc_id              = aws_vpc.main.id
+  cidr_block          = element(var.db_subnets_cidr,count.index)
+  availability_zone   = element(var.az, count.index)
+
+  tags = {
+    Name = "db_subnet-${count.index+1}"
   }
 }
 
