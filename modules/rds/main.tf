1fada6922b12e9c735bb0a03cfce705211306cf0
madhan-46 <madhanmohanreddyeeda@gmail.com>
2024-02-14 00:37:42 -0500

    commenting the db resource

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -4,17 +4,17 @@ resource "aws_db_parameter_group" "main" {
 }
 
 
-resource "aws_db_instance" "main" {
-  allocated_storage    = var.allocated_storage
-  db_name              = var.dbname
-  engine               = var.engine
-  engine_version       = var.engine_version
-  instance_class       = var.instance_class
-  username             = var.username
-  password             = var.password
-  parameter_group_name = aws_db_parameter_group.main.name
-  skip_final_snapshot  = true
-}
+#resource "aws_db_instance" "main" {
+#  allocated_storage    = var.allocated_storage
+#  db_name              = var.dbname
+#  engine               = var.engine
+#  engine_version       = var.engine_version
+#  instance_class       = var.instance_class
+#  username             = var.username
+#  password             = var.password
+#  parameter_group_name = aws_db_parameter_group.main.name
+#  skip_final_snapshot  = true
+#}
 
 
 
