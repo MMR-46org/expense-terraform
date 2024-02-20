7198f7cd44e48d4b8931c60837bff9871014b906
madhan-46 <madhanmohanreddyeeda@gmail.com>
2024-02-20 00:47:02 -0500

    fix errors

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -17,7 +17,7 @@ resource "aws_autoscaling_group" "main" {
   }
 
   tags = {
-    key                 = "name"
+    key                 = name
     value               = local.name
     propagate_at_launch = true
   }
