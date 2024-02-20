9199119fd3a9b819e082b72d21ae526acff67232
madhan-46 <madhanmohanreddyeeda@gmail.com>
2024-02-20 13:58:10 -0500

    updating the load balancer listener http redirecting to the https

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -22,9 +22,25 @@ resource "aws_lb_listener" "https" {
   default_action {
     type             = "forward"
     target_group_arn = var.target_group_arn
+
   }
 }
 
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
 
 
 
