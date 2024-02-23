6d0cb252dcd83a661e25667682041719fee0b981
madhan-46 <madhanmohanreddyeeda@gmail.com>
2024-02-23 02:34:09 -0500

    fixing error

--- modules/app/userdata.sh
+++ modules/app/userdata.sh
@@ -2,4 +2,4 @@
 
 yum install python3.11-pip -y  &>>/opt/userdata.log
 pip3.11 install botocore boto3  &>>/opt/userdata.log
-ansible-pull -i localhost, -U http://github.com/madhan-46/expense-ansible expense.yml -e service_name=${service_name} -e env=${env}  &>>/opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/madhan-46/expense-ansible expense.yml -e service_name=${service_name} -e env=${env}  &>>/opt/userdata.log
