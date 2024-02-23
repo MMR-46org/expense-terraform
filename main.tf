505ff69ec8157387d37fe6521de156a4ac7adbc5
madhan-46 <madhanmohanreddyeeda@gmail.com>
2024-02-23 03:18:03 -0500

    update parameter rds to the backend

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ module "backend" {
   sg_cidr_block              = lookup(lookup(var.vpc, "main", null), "app_subnets_cidr", null)
   vpc_id                     = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
   vpc_zone_identifier        = lookup(lookup(module.vpc, "main", null), "app_subnets_ids",  null)
-  parameters                 = ["rds"]
+  parameters                 = ["arn:aws:ssm:us-east-1:512646826903:parameter/${var.env}.${var.project_name}.rds.*"]
 }
 
 
