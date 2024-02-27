039ef31c89df9f8427af9127f986084447a19cb6
madhan-46 <madhanmohanreddyeeda@gmail.com>
2024-02-27 15:13:37 -0500

    networking concepts

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -46,7 +46,7 @@ resource "aws_subnet" "public" {
 
 resource "aws_route_table" "public" {
   count    = length(var.public_subnets_cidr)
-  vpc_id = aws_vpc.main.id
+  vpc_id   = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
