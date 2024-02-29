bd1236b94268fdd74aa8b5ed84746bd41b103b26
madhan-46 <madhanmohanreddyeeda@gmail.com>
2024-02-28 23:23:14 -0500

    kms key policy to encrypt the disk

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -13,16 +13,16 @@ resource "aws_launch_template" "main" {
     name = aws_iam_instance_profile.main.name
   }
 
-#  block_device_mappings {
-#    device_name = "/dev/sda1"
-#
-#    ebs {
-#      volume_size = 10
-#      encrypted   = true
-#      kms_key_id  = var.kms_key_id
-#      delete_on_termination = true
-#    }
-#  }
+  block_device_mappings {
+    device_name = "/dev/sda1"
+
+    ebs {
+      volume_size = 10
+      encrypted   = true
+      kms_key_id  = var.kms_key_id
+      delete_on_termination = true
+    }
+  }
 
 }
 
