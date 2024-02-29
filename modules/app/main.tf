5f881b5cf443a229132c16caa8354d374fce5088
madhan-46 <madhanmohanreddyeeda@gmail.com>
2024-02-29 12:59:46 -0500

    kms key policy to encrypt the disk

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -159,44 +159,4 @@ resource "aws_iam_instance_profile" "main" {
 
 
 
-resource "aws_kms_key" "main" {
-  key_id = aws_kms_key.main.id
-  policy = jsonencode({
-    Id = "example"
-    Statement = [
-      {
-        "Sid": "kmskeyPolicy",
-        "Effect": "Allow",
-        "Action": [
-        "kms:Encrypt",
-        "kms:Decrypt",
-        "kms:ReEncrypt*",
-        "kms:GenerateDataKey*",
-        "kms:DescribeKey"
-      ],
-        "Resource": concat ([
-        "arn:aws:iam::512646826903:instance-profile/dev-expense-backend-role",
-        "arn:aws:iam::512646826903:instance-profile/dev-expense-frontend-role"
-      ])
-      },
-      {
-        "Sid": "kmsCreateGrant",
-        "Effect": "Allow",
-        "Action": [
-          "kms:CreateGrant"
-        ],
-        "Resource": concat ([
-          "arn:aws:iam::512646826903:instance-profile/dev-expense-backend-role",
-          "arn:aws:iam::512646826903:instance-profile/dev-expense-frontend-role"
-        ]),
-        "Condition": {
-          "Bool": {
-            "kms:GrantIsForAWSResource": true
-          }
-        }
-      },
 
-    ]
-    Version = "2012-10-17"
-  })
-}
